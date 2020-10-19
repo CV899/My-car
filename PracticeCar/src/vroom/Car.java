9a22c47a315af2255b90eca70e7475b3c6da9fd1
Christian Vincent <christian.vincent25@hotmail.com>
2020-10-19 14:42:23 -0400

    init

---
+++ PracticeCar/src/vroom/Car.java
@@ -0,0 +1,5 @@
+package vroom;
+
+public class Car {
+
+}
---
+++ PracticeCar/src/vroom/Simulator.java
@@ -0,0 +1,10 @@
+package vroom;
+
+public class Simulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
